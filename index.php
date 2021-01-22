4ff0478bdb02778c91160c17ae7b6be4ab901c1b
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-01-22 16:47:39 +0100

    css structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Dischi</title>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            
+        </div>
+    </header>
+    <main> 
+        <div class="wrapper">
+            <div class="album_container">
+                <div class="thumbnail_container">
+                    <img src="img/linkin-park.jpg" alt="">
+                </div>
+                <h2>Castel of Glass</h2>
+                <h3>Linkin Park</h3>
+                <h4>2012</h4>
+            </div>
+            <div class="album_container">
+                
+            </div>
+            <div class="album_container">
+                
+            </div>
+        </div>
+    </main>
+</body>
+</html>
