c472b25b5797b10ac5aba4b08a7ab56dbac38b8a
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-01-22 18:58:39 +0100

    fix broken container

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'db.php'; ?>
+<?php include __DIR__ . '/db.php'; ?>
 
 
 <!DOCTYPE html>
@@ -20,11 +20,11 @@
             <?php foreach ($albums as $album) { ?>
             <div class="album_container">
                 <div class="thumbnail_container">
-                    <img src=<?php echo $album[poster]; ?> alt="poster">
+                    <img src="<?php echo $album['poster']; ?>" alt="poster">
                 </div>
-                <h2><?php echo $album[title]; ?></h2>
-                <h3><?php echo $album[author]; ?></h3>
-                <h4><?php echo $album[year]; ?></h4>
+                <h2><?php echo $album['title']; ?></h2>
+                <h3><?php echo $album['author']; ?></h3>
+                <h4><?php echo $album['year']; ?></h4>
             </div>
             <?php } ?>
         </div>
