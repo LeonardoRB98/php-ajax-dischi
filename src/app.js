cd7ebbce08b0d835b409650a775e030b6e751f53
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-01-25 15:43:22 +0100

    axios installation via npm

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-
+const axios = require('axios').default;
+// https://flynn.boolean.careers/exercises/api/array/music
 var app = new Vue (
     {
         el: '#app',
@@ -7,7 +8,10 @@ var app = new Vue (
             prova: 'ciao'
         },
         mounted: function() {
-            alert('ciao');
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(function(response){
+                console.log(response.data.response);
+            });
         }
     }
 );
